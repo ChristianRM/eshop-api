f50385665d82dc45d41ddd52565d033a714e58af
ChrisRM <chris.ricardo.r.m@gmail.com>
2020-10-22 18:20:19 -0500

    :bookmark: Initialize project

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const { graphqlHTTP } = require("express-graphql")
+const graphqlSchema = require("./graphql/schema")
+const graphqlResolvers = require("./graphql/resolvers")
+
+const app = express()
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
+    graphiql: true,
+  })
+)
+
+app.listen(3000, 
+    () => console.log("Server is running on localhost:3000"))
