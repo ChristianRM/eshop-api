cdba743bb78f251dd017bf72465a37d9b036e18f
ChrisRM <chris.ricardo.r.m@gmail.com>
2020-10-22 19:27:42 -0500

    :bookmark: Connect to DB

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const { graphqlHTTP } = require("express-graphql")
 const graphqlSchema = require("./graphql/schema")
 const graphqlResolvers = require("./graphql/resolvers")
@@ -14,5 +15,11 @@ app.use(
   })
 )
 
-app.listen(3000, 
-    () => console.log("Server is running on localhost:3000"))
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.qlfc7.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose
+    .connect(uri, options)
+    .then(() => app.listen(3000, console.log("Server is running")))
+    .catch(error => {
+        throw error
+    })
